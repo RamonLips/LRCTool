967fef1150e56d30aafba04667ce334fb5e3a075
RowanWays <rowanaalders06@gmail.com>
2025-02-25 14:30:33 +0100

    Updates

--- src/js/data.js
+++ src/js/data.js
@@ -9,6 +9,11 @@ document.addEventListener('DOMContentLoaded', function () {
             findInfo(input)
         }
     })
+
+    const globalToggle = document.querySelector('#globalToggle')
+    globalToggle.addEventListener('change', () => {
+        // TODO: Implement toggle only for popup for now. 
+    })
 })
 
 function findInfo(input) {
@@ -31,62 +36,46 @@ function findInfo(input) {
         }
     })
     .then(data => {
-        resultsHeader.innerHTML = `Results: ${data.length}`
+        resultsHeader.innerHTML = `Results for "${input}" (${data.length})`
 
         data.forEach(element => {
             // console.log(element)
             const trackName = element.trackName
             const artistName = element.artistName
             const plainLyrics = element.plainLyrics
-            const syncedLyrics = element.syncedLyrics
             const durationSEC = element.duration
             const minutes = Math.floor(durationSEC / 60)
             const seconds = durationSEC % 60
             const formattedDuration = `${minutes}:${seconds.toString().padStart(2, '0')}`
             const trackID = element.id
 
-            if (plainLyrics !== null && syncedLyrics !== null) {
+            if (plainLyrics !== null) {
                 const div = document.createElement('div')
                 div.className = 'resultItem'
+                div.dataset.plainLyrics = plainLyrics
+                div.addEventListener('click', () => {
+                    openSong(trackID)
+                })
 
-                const title = document.createElement('h2')
-                if (!trackName.includes(artistName)) {
-                    title.innerHTML = `<span>${trackID}</span> - ${trackName} - ${artistName} - ${formattedDuration}`
-                }
-                else {
-                    title.innerHTML = `<span>${trackID}</span> - ${trackName} - ${formattedDuration}`
-                }
+                const title = document.createElement('div')
+                title.className = 'info'
+                title.innerHTML = `<div>
+                                       <p class="ID">${trackID}</p>
+                                       <span class="divider"> - </span>
+                                       <p class="duration">${formattedDuration}</p>
+                                   </div>
+                                   <h2 class="name">${trackName}</h2>
+                                   ${trackName.includes(artistName) ? '' : `<h3 class="artist">${artistName}</h3>`}
+                                   `
                 div.appendChild(title)
+
+                let hr = document.createElement('hr')
+                div.appendChild(hr)
                 
                 let lyrics = document.createElement('p')
-                lyrics.innerHTML = plainLyrics.replace(/\n/g, '<br>')
+                lyrics.className = 'lyrics'
+                lyrics.innerHTML = plainLyrics.split('\n').slice(0, 5).join('<br>') + '...'
                 div.appendChild(lyrics)
-
-                const toggleSwitch = document.createElement('label')
-                toggleSwitch.className = 'switch'
-                const input = document.createElement('input')
-                input.type = 'checkbox'
-                const span = document.createElement('span')
-                span.className = 'slider round'
-                toggleSwitch.appendChild(input)
-                toggleSwitch.appendChild(span)
-                div.appendChild(toggleSwitch)
-
-                input.addEventListener('change', () => {
-                    if (input.checked) {
-                        lyrics.innerHTML = syncedLyrics.replace(/\n/g, '<br>')
-                    } else {
-                        lyrics.innerHTML = plainLyrics.replace(/\n/g, '<br>')
-                    }
-                })
-
-                const copyButton = document.createElement('button')
-                copyButton.textContent = 'Copy Lyrics'
-                copyButton.addEventListener('click', () => {
-                    const textToCopy = input.checked ? syncedLyrics : plainLyrics
-                    navigator.clipboard.writeText(textToCopy)
-                })
-                div.appendChild(copyButton)
 
                 resultsContainer.appendChild(div)
             }
